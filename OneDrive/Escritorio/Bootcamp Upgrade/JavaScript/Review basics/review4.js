8f7ad94a10ca5806bc2291011e258ffc1938e6f5
AlvaroGutierrez96 <alvaro.gutierrez@bootcamp-upgrade.com>
2022-03-03 23:40:49 +0100

    Iteration#4

---
+++ OneDrive/Escritorio/Bootcamp Upgrade/JavaScript/Review basics/review4.js
@@ -0,0 +1,11 @@
+/* Crea una función llamada findArrayIndex que reciba como parametros un array de textos y un texto y devuelve la posición del array
+cuando el valor del array sea igual al valor del texto que enviaste como parametro. Haz varios ejemplos y compruebalos. */
+
+const findArrayIndex = (array,text) => {
+    for (const item of array) {
+        if(text == item){
+            console.log(array.indexOf(item))
+        }
+    }
+}
+
